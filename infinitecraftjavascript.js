ce1cfde03be4b2192cf88d1a6331386f0b1a2928
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2024-05-19 19:38:30 -0400

    Create infinitecraftjavascript.js

---
+++ infinitecraftjavascript.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = 'https://gr4vity.pages.dev/infinite'; //set url
+        
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
+
+// Remove the isUrl function as it's not needed anymore
