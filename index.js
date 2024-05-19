23317174b6c100e6bfe88cbee923e98bb54e2c79
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2024-05-19 19:29:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    const input = document.querySelector('input');
 
-form.addEventListener('submit', async event => {
-    event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        let url = 'https://gr4vity.pages.dev/portal'; // Set the URL to always use "https://gr4vity.pages.dev/portal"
-        
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    form.addEventListener('submit', async event => {
+        event.preventDefault();
+        window.navigator.serviceWorker.register('./sw.js', {
+            scope: __uv$config.prefix
+        }).then(() => {
+            let url = input.value; // Get the URL from the input element
+            
+            window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        });
     });
 });
-
-// Remove the isUrl function as it's not needed anymore
