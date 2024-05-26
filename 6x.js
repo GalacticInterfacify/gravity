edb100d63092547b534b50e65cf307600ed037ce
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2024-05-26 00:36:45 -0400

    Update 6x.js

--- 6x.js
+++ 6x.js
@@ -6,10 +6,8 @@ form.addEventListener('submit', async event => {
     window.navigator.serviceWorker.register('./sw.js', {
         scope: __uv$config.prefix
     }).then(() => {
-        let url = 'https://gr4vity.pages.dev/redirecteaster'; // Set the URL to always use "https://gr4vity.pages.dev/portal"
-        
+        let url = 'https://6xgames.com';
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
 });
 
-// Remove the isUrl function as it's not needed anymore
