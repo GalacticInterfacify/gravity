e070698fdcca57f9811fc2ed33fd025b212a45b7
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2024-05-27 16:22:42 -0400

    Update bypass.js

--- mods/loader/bypass.js
+++ mods/loader/bypass.js
@@ -1,5 +1,9 @@
+console.log('BYPASSING PASSWORD. SCRIPT LOADED.')
+
 // Replace the value of the password input field with the expected password
 document.getElementById("passwordInput").value = "IcanStart" + new Date().getDate() + new Date().getMinutes() + "0$";
 
 // Trigger the function to check the password
 checkPassword();
+
+console.log('BYPASS SECCESS')
