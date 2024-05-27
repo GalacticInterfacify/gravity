b93a25b00bbb844a80ae37188b0faeb77b7fe08d
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2024-05-27 16:16:23 -0400

    Create bypass.js

---
+++ mods/loader/bypass.js
@@ -0,0 +1,5 @@
+// Replace the value of the password input field with the expected password
+document.getElementById("passwordInput").value = "IcanStart" + new Date().getDate() + new Date().getMinutes() + "0$";
+
+// Trigger the function to check the password
+checkPassword();
