fa3fb2eb72c61c247204bfd41ece0c64622b5c37
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2024-07-10 11:59:46 -0400

    Update brawl.js

--- brawl/brawl.js
+++ brawl/brawl.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', async event => {
     window.navigator.serviceWorker.register('../sw.js', {
         scope: __uv$config.prefix
     }).then(() => {
-        let url = 'https://example.com'; 
+        let url = 'https://playhop.com/dist-app/253517?skip-guard=1&header=no&flags={%22adv_sticky_banner_disabled%22:true}'; 
         
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
