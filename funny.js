44728a2276cdfc5760b02ac7ea0b7c233b9c63d2
Galactic Interfaces <143354205+GalacticInterfaces@users.noreply.github.com>
2024-10-04 19:18:58 -0400

    Update funny.js

--- funny.js
+++ funny.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', async event => {
     window.navigator.serviceWorker.register('./sw.js', {
         scope: __uv$config.prefix
     }).then(() => {
-        let url = 'https://rawcdn.githack.com/nightrose-labs/quartz/master/1/6af3cc65-32f9-4f3d-a66c-6c8882a7c5df/index.html'; 
+        let url = 'https://html5.gamedistribution.com/a1b7ac4025214a5cb0bd01a4f675513a/'; 
         
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
