bea3d067571fc6e45a88f85197921ce97dabee91
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2024-10-24 15:07:25 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://gravitybareworkerserver.godoukle.workers.dev/',
+    bare: 'https://bare.benrogo.net/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
