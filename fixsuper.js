d468718e2fac632c30bcef6932a817612e341609
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2024-11-12 16:09:23 -0500

    Update fixsuper.js

--- fixsuper.js
+++ fixsuper.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async event => {
     window.navigator.serviceWorker.register('./sw.js', {
         scope: __uv$config.prefix
     }).then(() => {
-        let url = 'https://gr4vity.pages.dev/portal'; // Set the URL to always use "https://gr4vity.pages.dev/portal"
+        let url = 'https://gr4v1ty.pages.dev/portal'; // Set the URL to always use "https://gr4vity.pages.dev/portal"
         
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
