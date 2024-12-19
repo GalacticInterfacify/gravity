e9645f5f6fc939bb686958a6b601e361c76bc864
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2024-12-19 17:32:08 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://gointerstellar.app/outerspace/',
+    bare: 'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
