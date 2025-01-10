aeea78905df7cf3f01957ee96046c70c84f6cab9
GalacticInterfacify <168484222+GalacticInterfacify@users.noreply.github.com>
2025-01-09 22:00:43 -0500

    Create raghit.js

---
+++ raghit.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = 'https://freetoplayz.github.io/ragdoll-hit/'; 
+        
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
